5e826cb3e359117f65572eaf59b29e76ce78ef8d
mikecebul <mikecebul@gmail.com>
2023-11-10 15:36:02 -0500

    Chore: add navigation

---
+++ src/_components/main-nav.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn, isActiveRoute } from "@/_lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/_components/ui/navigation-menu";
+import { usePathname } from "next/navigation";
+import { siteConfig } from "@/_config/site";
+import { ModeToggle } from "./mode-toggle";
+
+export function MainNav() {
+  const currentPathName = usePathname();
+
+  return (
+    <div className="flex justify-center items-center py-8 container relative">
+      <p className="text-xl font-bold absolute left-8">MikeCebul</p>
+      <NavigationMenu className="justify-around">
+        <NavigationMenuList>
+          {siteConfig.NavLinks.map((item) => (
+            <NavigationMenuItem key={item.title}>
+              <Link href={item.href} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={cn(navigationMenuTriggerStyle(), {
+                    "": isActiveRoute(currentPathName as string, item.href),
+                  })}
+                >
+                  {item.title}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="absolute right-8">
+        <ModeToggle />
+      </div>
+    </div>
+  );
+}
---
+++ src/_config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+NavLinks: [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Projects",
+        href: "/projects",
+    }
+]
+}
